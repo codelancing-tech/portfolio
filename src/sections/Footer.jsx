388f2415fc321946ae172c98945b53281a030549
codelancing-tech <codelancingtechnologies@gmail.com>
2025-03-06 16:36:03 +0530

    the social media links are updated.

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -17,19 +17,23 @@ const Footer = () => {
        </a>
     </div>
         
-       
+    <a href="http://www.linkedin.com/in/codelancing-technologies-339b31336" target="_blank" rel="noopener noreferrer">
         <div className="social-icon hover:bg-sky-400">
           <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
           </div>
+          </a>
         
           <a href="mailto:codelancingtechnologies@gmail.com" target="_blank">
            <div className="social-icon w-full h-full hover:bg-sky-400">
           <img src="/assets/twitter.svg" alt="twitter" className="w-1/2 h-1/2" />
            </div>
           </a>
+          <a href="https://www.instagram.com/codelancing_technologies_/" target="_blank" rel="noopener noreferrer">
         <div className="social-icon hover:bg-sky-400">
           <img src="/assets/instagram.svg" alt="instagram" className="w-1/2 h-1/2" />
         </div>
+        </a>
+        
       </div>
 
       <p className="text-white-500">© Codelancing Technologies. All rights reserved.</p>
